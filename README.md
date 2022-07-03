5b2ffe37447242ec47a08006d30a24735cb888d7
21stChrom <parkerjmartin2020@gmail.com>
2022-07-02 23:53:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-5bhddq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-5bhddq)
---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./style.css";
+import {FaHome} from '@react-icons/all-files/fa/FaHome'
+import {BsFillDropletFill} from '@react-icons/all-files/bs/BsFillDropletFill'
+
+export default function App() {
+  return (
+    <div className="container"> 
+    <div  className="img" >
+    <a href="https://developer.mozilla.org/" ><img style={{marginLeft:"80px"}} 
+    src="https://ghavatars.staticblitz.com/21stChrom.png?size=160" alt="21stChrom profile picture" /></a>
+    </div>
+    <div style={{color:"red"}}>``    Juice - No Laces</div> 
+
+    <audio style={{backgroundColor: "transparent"}} controls>
+    <source src="https://cdn.jsdelivr.net/gh/21stChrom/react-1jl7rd@main/src/Juice%20mus/Juice%20WRLD%20-%20No%20Laces%20(Unreleased)%20(Lyrics)%20[720p].mp3" type="audio/mpeg" />
+      </audio> <div style={{color:"red"}}>``    Juice - No Laces</div> 
+    <audio style={{backgroundColor: "transparent"}} controls>
+    <source src="https://cdn.jsdelivr.net/gh/21stChrom/react-1jl7rd@main/src/Juice%20mus/Juice%20WRLD%20-%20No%20Laces%20(Unreleased)%20(Lyrics)%20[720p].mp3" type="audio/mpeg" />
+      </audio>
+      <div style={{color:"red"}}>``    Juice - No Laces</div> 
+    <audio style={{backgroundColor: "transparent"}} controls>
+    <source src="https://cdn.jsdelivr.net/gh/21stChrom/react-1jl7rd@main/src/Juice%20mus/Juice%20WRLD%20-%20No%20Laces%20(Unreleased)%20(Lyrics)%20[720p].mp3" type="audio/mpeg" />
+      </audio>
+      <div style={{color:"red"}}>``    Juice - No Laces</div> 
+    <audio style={{backgroundColor: "transparent"}} controls>
+    <source src="https://cdn.jsdelivr.net/gh/21stChrom/react-1jl7rd@main/src/Juice%20mus/Juice%20WRLD%20-%20No%20Laces%20(Unreleased)%20(Lyrics)%20[720p].mp3" type="audio/mpeg" />
+      </audio>
+      <div style={{color:"red"}}>``    Juice - No Laces</div> 
+    <audio style={{backgroundColor: "transparent"}} controls>
+    <source src="https://cdn.jsdelivr.net/gh/21stChrom/react-1jl7rd@main/src/Juice%20mus/Juice%20WRLD%20-%20No%20Laces%20(Unreleased)%20(Lyrics)%20[720p].mp3" type="audio/mpeg" />
+      </audio>
+
+   <div className="second" style={{color: "transparent", marginTop: "-31px"}}><strong>Parker made this ... Just sayin.</strong></div>
+   {
+      <div className="first" style={{color: "red"}}>
+      <div className="bb" style={{margin: "100px 10px 10px 10px"}} />
+      <div  className="Pmack">
+        <div><BsFillDropletFill /></div>
+       <div className="Pmack"><span className="bibop" style={{color: "red"}}><BsFillDropletFill /></span>
+       </div>
+       </div>
+      </div>
+ }
+ <h1 className="animation">
+   <button style={{backgroundColor: "transparent", border: "3px solid transparent"}}><a href="https://stackblitz.com/edit/react-d2pjuj?file=public%2Findex.html,src%2FApp.js/"><FaHome /></a></button>
+   </h1>
+  
+
+    </div>
+  );
+}
